f3b92d286f514d7f8a7279596c33c52fe85fab6e
Gilvan <gilvanritter@gmail.com>
2019-10-11 03:09:07 -0300

    Init server.

---
+++ cmd/maskada-api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gritt/maskada/core"
+	"github.com/gritt/maskada/details"
+	"github.com/gritt/maskada/details/db"
+	"github.com/gritt/maskada/details/rest"
+)
+
+// TODO @gritt: Test this!
+func main() {
+	cfg, err := details.NewConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	r, err := db.NewRepository(cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	uc := core.NewCreateTransactionUseCase(r)
+
+	// TODO @gritt: Use Dependecy Injection
+	api := rest.NewAPI(uc)
+
+	server := &http.Server{
+		Addr:    ":8888",
+		Handler: api.Routes(),
+	}
+
+	if err = server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalln(err)
+	}
+}
